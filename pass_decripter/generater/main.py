074385da7bd6428b32032bf0cce3144110c0db3a
J-A-$-0-N <62867830+J-A-S-0-N@users.noreply.github.com>
2020-06-27 22:13:10 +0800

    Add files via upload

---
+++ pass_decripter/generater/main.py
@@ -0,0 +1,81 @@
+import random
+from os import system
+from time import sleep
+
+alpabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+
+def scramble():
+    global a
+    global b
+    global c
+    global d 
+    global e 
+    global f 
+    global g
+    global h
+    global i
+    global j
+    global k
+    global l
+    global m
+    global n
+    global o
+    global p
+    global q
+    global r
+    global s
+    global t
+    global u
+    global v
+    global w
+    global x
+    global y
+    global z
+    a = "b"
+    b = "c"
+    c = "d"
+    d = "e"
+    e = "f"
+    f = "g"
+    g = "h"
+    h = "i"
+    i = "j"
+    j = "k"
+    k = "l"
+    l = "m"
+    m = "n"
+    n = "o"
+    o = "p"
+    p = "q"
+    q = "r"
+    r = "s"
+    s = "t"
+    t = "u"
+    u = "v"
+    v = "w"
+    w = "x"
+    x = "y"
+    y = "z"
+    z = "a"
+    mixedalpabet = [b, c, d, e, f,g,h,i,j,k,l,m, n,o,p,q,r,s,t,u,v, w, x, y, z, a]
+
+def main():
+    
+    if sel1 == 2:
+    if sel1 == 1:
+        print("this is password generater")
+        print("enter the word you want to encript")
+        word = input(">>> ")
+        for i in range(int(words)):
+
+        
+                
+            
+            
+            
+    
+    
+
+if __name__ == "__main__":
+    main() 
