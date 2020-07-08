d48c48849fc35ced6ae8531cd02ef1287ba43434
J-A-$-0-N <62867830+J-A-S-0-N@users.noreply.github.com>
2020-07-08 22:17:42 +0800

    Add files via upload

---
+++ Longest Word'/main.py
@@ -0,0 +1,21 @@
+#Have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty.
+#Examples
+#Input: "fun&!! time"
+#Output: time
+#Input: "I love dogs"
+#Output: love
+
+def LongestWord(sen):
+    senlis = list(sen)
+    senlen = len(sen)
+    eachword = {}
+    x = 0
+    for i in range(0,senlen):
+        d["string{0}".format(i)] = senlis[i - 1]
+    for i in range(0.senlen):
+        
+    # code goes here
+    # return sen
+
+# keep this function call here 
+print(LongestWord("hello my name is blabla"))
