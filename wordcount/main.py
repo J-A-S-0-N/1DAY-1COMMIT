9bcc63ef2c3bbca8da08e5d5b42e7e5b86289fd7
J-A-$-0-N <62867830+J-A-S-0-N@users.noreply.github.com>
2020-07-21 22:26:44 +0800

    Add files via upload

---
+++ wordcount/main.py
@@ -0,0 +1,10 @@
+def main():
+    string = input("Enter any string to count word: ");
+    word_length = len(string.split());
+    print("\nNumber of words =",word_length);
+
+
+
+if __name__ == "__main__":
+    main()
+    
